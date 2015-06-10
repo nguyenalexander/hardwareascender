5612de9863cc6b660be733897fe60cae97f2db3e
Alexander Nguyen <nguyen.h.alexander@gmail.com>
2015-06-10 11:56:43 -0700

    watchlist fix

--- api/models/User.js
+++ api/models/User.js
@@ -26,8 +26,7 @@ module.exports = {
       via: 'user'
     },
     watchList : {
-      collection: 'Listing',
-      via: 'user'
+      collection: 'Listing'
     },
     messages: {
       collection: 'Message',
